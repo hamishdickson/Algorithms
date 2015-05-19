bba3a0a155a6e4bae7ce826837f41c5b75260c76
hamishdickson <hamish.dickson@gmail.com>
2015-05-19 08:53:51 +0100

    first commit

---
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/App.java
@@ -0,0 +1,13 @@
+package JavaAlgorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
