2cef01686295870060c93561b4f4d983abe90950
hamishdickson <hamish.dickson@gmail.com>
2015-05-22 10:55:51 +0100

    add a nice lorentz transform

--- PythonAlgorithms/test.py
+++ PythonAlgorithms/test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+import math
+
+# sod it: c=e=1
+c = 1
+
 # some comment
 def printme(str):
     "This prints a passed string"
@@ -15,7 +20,16 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+# this is a first attempt
+def oneDimLorentzTransformation(x, t, v):
+    """A boost in the x-dir only"""
+    beta = v / c
+    gamma = 1 / (1 - math.sqrt(beta))
+    return gamma * ( x - v*t)
+
+
 alist = [10, 20, 30]
 printme(alist)
 printme("hamish")
 printme(fibonacci(10))
+print oneDimLorentzTransformation(1, 1, 0.34)
