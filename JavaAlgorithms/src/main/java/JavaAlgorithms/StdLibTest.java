a8a060d0aacfc09713f79fd9a1a90e1102c87759
hamishdickson <hamish.dickson@gmail.com>
2015-06-07 17:32:12 +0100

    pretty picture

--- JavaAlgorithms/src/main/java/JavaAlgorithms/StdLibTest.java
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/StdLibTest.java
@@ -1,7 +1,10 @@
 package JavaAlgorithms;
 
+import edu.princeton.cs.introcs.StdDraw;
 import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.StdRandom;
+
+import java.util.Arrays;
 
 /**
  * StdLib is a library provided by the nice Algorithms floke at princeton - it lets you import/export stuff, create
@@ -14,6 +17,9 @@ public class StdLibTest {
 
         String[] a = {"5", "100.0", "200.0"};
         stdLibTest.randomSequence(a);
+
+        // nice picture
+        stdLibTest.sortedPlot();
     }
 
     // example of stdout
@@ -25,6 +31,23 @@ public class StdLibTest {
         for (int i = 0; i < N; i++) {
             double x = StdRandom.uniform(lo, hi);
             StdOut.printf("%.2f\n", x);
+        }
+    }
+
+    // drawing
+    public void sortedPlot() {
+        int N = 50;
+        double[] a = new double[N];
+        for (int i = 0; i < N; i++)
+            a[i] = StdRandom.uniform();
+        Arrays.sort(a);
+
+        for (int i = 0; i < N; i++) {
+            double x = 1.0*i/N;
+            double y = a[i]/2.0;
+            double rw = 0.5/N;
+            double rh = a[i]/2.0;
+            StdDraw.filledRectangle(x, y, rw, rh);
         }
     }
 }
