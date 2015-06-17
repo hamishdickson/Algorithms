d70dc7451d1d872f4024e8060c98189afd01f9dd
hamishdickson <hamish.dickson@gmail.com>
2015-06-17 08:49:13 +0100

    add dijkstras test

---
+++ JavaAlgorithms/src/test/java/JavaAlgorithms/Section1/DijkstraTest.java
@@ -0,0 +1,17 @@
+package JavaAlgorithms.Section1;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by hamishdickson on 17/06/15.
+ */
+public class DijkstraTest extends TestCase {
+    @Test
+    public void evaluateSimpleExpression() {
+        String expression = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
+        Dijkstra dijkstra = new Dijkstra();
+
+        assertEquals(101, dijkstra.dijkstra(expression));
+    }
+}
