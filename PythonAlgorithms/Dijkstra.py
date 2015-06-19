f5d78055792903e292fdad59da2f3daaa4cf0183
hamishdickson <hamish.dickson@gmail.com>
2015-06-19 08:35:47 +0100

    more dijkstra

--- PythonAlgorithms/Dijkstra.py
+++ PythonAlgorithms/Dijkstra.py
@@ -1,3 +1,10 @@
 def dijkstra(in):
     """dijkstra's algorithm for evaluating string expressions"""
+    ops = []
+    vals = []
+
+    parts = in.split()
     
+    for i in range (0, len(in)):
+        s = parts[i]
+        
