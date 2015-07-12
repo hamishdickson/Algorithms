f343a2e2d1ab84747b1837a642e13aaac6c888dd
hamishdickson <hamish.dickson@gmail.com>
2015-07-12 17:07:57 +0100

    add pushdown stack

---
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/LinkedList/Node.java
@@ -0,0 +1,9 @@
+package JavaAlgorithms.Section1.LinkedList;
+
+/**
+ * Created by hamishdickson on 12/07/15.
+ */
+public class Node<Item> {
+    Item item;
+    Node next;
+}
---
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/LinkedList/PushdownStack.java
@@ -0,0 +1,38 @@
+package JavaAlgorithms.Section1.LinkedList;
+
+/**
+ * pushdown stack
+ *
+ * implemented with a linked list
+ *
+ * Keep track of size with numberOfItems
+ */
+public class PushdownStack<Item> {
+    private Node first; // top of stack - most recently added node
+    private int numberOfItems;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return numberOfItems;
+    }
+
+    // add item to top of stack
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        numberOfItems++;
+    }
+
+    // remove item from top of stack
+    public Item pop() {
+        Item item = (Item) first.item;
+        first = first.next;
+        numberOfItems--;
+        return item;
+    }
+}
