1822bb65136b6270112ebae95e265a5f7de96e81
hamishdickson <hamish.dickson@gmail.com>
2015-07-12 17:29:06 +0100

    add fifo queue comments

--- JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/LinkedList/FifoQueue.java
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/LinkedList/FifoQueue.java
@@ -2,20 +2,22 @@ package JavaAlgorithms.Section1.LinkedList;
 
 /**
  * Created by hamishdickson on 12/07/15.
+ *
  */
 public class FifoQueue<Item> {
-    private Node first;
-    private Node last;
-    private int N;
+    private Node first; // link to least recently added node
+    private Node last; // link to most recently added node - don't need this in a stack
+    private int numberOfItems;
 
     public boolean isEmpty() {
         return first == null;
     }
 
     public int size() {
-        return N;
+        return numberOfItems;
     }
 
+    // add item to end of list
     public void enqueue(Item item) {
         Node oldLast = last;
         last = new Node();
@@ -25,13 +27,14 @@ public class FifoQueue<Item> {
             first = last;
         else
             oldLast.next = last;
-        N++;
+        numberOfItems++;
     }
 
+    // remove item from beginnning of list
     public Item dequeue() {
         Item item = (Item) first.item;
         first = first.next;
-        N--;
+        numberOfItems--;
         if (isEmpty()) last = null;
         return item;
     }
