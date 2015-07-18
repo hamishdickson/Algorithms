f39e498fa7f814cf76e1ef38fc6335546dd4a218
hamishdickson <hamish.dickson@gmail.com>
2015-07-18 15:07:19 +0100

    add FixedCapacityStackOfStrings

---
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/FixedCapacityStackOfStrings.java
@@ -0,0 +1,31 @@
+package JavaAlgorithms.Section1;
+
+/**
+ * Created by hamishdickson on 18/07/15.
+ *
+ * Note created to do exercise
+ */
+public class FixedCapacityStackOfStrings {
+    private String[] a;
+    private int N;
+
+    public FixedCapacityStackOfStrings(int cap) {
+        a = new String[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(String item) {
+        a[N++] = item;
+    }
+
+    public String pop() {
+        return a[--N];
+    }
+}
