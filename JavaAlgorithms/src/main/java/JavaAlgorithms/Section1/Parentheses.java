7f1f8b5b8a68b8b9d3eef24fc7abc119549b42e7
hamishdickson <hamish.dickson@gmail.com>
2015-07-22 20:14:42 +0100

    more refactoring

--- JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/Parentheses.java
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/Parentheses.java
@@ -6,10 +6,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parentheses {
-    private Map<String, String> pairs = new HashMap<>();
+    private static final Map<String, String> pairsOfBrackets;
+
+    static {
+        pairsOfBrackets = new HashMap<>();
+        pairsOfBrackets.put(")", "(");
+        pairsOfBrackets.put("}", "{");
+        pairsOfBrackets.put("]", "[");
+    }
 
     public boolean isBalanced(String testString) {
-        buildPairsMap();
         PushdownStack<String> pushdownStack = new PushdownStack<>();
 
         for (int i = 0; i < testString.length(); i++) {
@@ -21,24 +27,14 @@ public class Parentheses {
                 if(isUnbalanced(pushdownStack, s)) return false;
             }
         }
-        
+
         return pushdownStack.isEmpty();
     }
 
     private boolean isUnbalanced(PushdownStack<String> pushdownStack, String s) {
         String lastOpening = pushdownStack.pop();
-        String newClosing = pairs.get(s);
-        return doesTheClosingBracketMatchAnOpeningBracket(lastOpening, newClosing);
-    }
-
-    private boolean doesTheClosingBracketMatchAnOpeningBracket(String last, String lastPair) {
-        return !lastPair.equals(last);
-    }
-
-    private void buildPairsMap() {
-        pairs.put(")", "(");
-        pairs.put("}", "{");
-        pairs.put("]", "[");
+        String newClosing = pairsOfBrackets.get(s);
+        return !newClosing.equals(lastOpening);
     }
 
     private boolean isOpeningBracket(String bracket) {
