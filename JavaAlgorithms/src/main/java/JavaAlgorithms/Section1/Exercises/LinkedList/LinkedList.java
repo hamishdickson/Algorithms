6901efcc3b3232078a68dbbfe073048b74b90fa8
hamishdickson <hamish.dickson@gmail.com>
2015-07-25 13:52:47 +0100

    pretty ugly, but works

--- JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/Exercises/LinkedList/LinkedList.java
+++ JavaAlgorithms/src/main/java/JavaAlgorithms/Section1/Exercises/LinkedList/LinkedList.java
@@ -15,14 +15,17 @@ public class LinkedList<Item> {
             first.item = item;
 
             last = first;
+        } else if (numberOfItems == 1) {
+            last = new Node();
+            first.next = last;
             last.item = item;
         } else {
             Node oldLast = last;
             last = new Node();
             last.item = item;
-            last.next = oldLast;
-            numberOfItems++;
+            oldLast.next = last;
         }
+        numberOfItems++;
     }
 
     public Item peek() {
@@ -33,6 +36,14 @@ public class LinkedList<Item> {
      * created for 1.3.19
      */
     public void removeLast() {
+        Node position = first;
+        for (int i = 0; i < numberOfItems - 2; i++) {
+            position = position.next;
+        }
 
+        numberOfItems--;
+        position.next = null;
+
+        last = position;
     }
 }
